f5a76bdb95d12ea9994a8182a6e512a228fef8c5
John Inacay <jinac@users.noreply.github.com>
2018-03-25 00:42:01 -0700

    Update for gan

--- blocks.py
+++ blocks.py
@@ -10,8 +10,9 @@ from keras.layers.normalization import BatchNormalization
 
 def bn_dense(inputs, out_dim,
              activation='relu',
-             dropout_rate=None):
-    out = Dense(out_dim)(inputs)
+             dropout_rate=None,
+             use_bias=True):
+    out = Dense(out_dim, use_bias=use_bias)(inputs)
     out = BatchNormalization()(out)
     if activation is not None:
         out = Activation(activation)(out)
@@ -23,23 +24,33 @@ def bn_dense(inputs, out_dim,
 def bn_conv_layer(inputs, num_filters, kernel_size,
                   strides=[1, 1], padding='same',
                   activation='relu',
+                  batchnorm=True,
                   dropout_rate=None):
     out = Conv2D(num_filters, kernel_size,
-                 strides=strides, padding=padding)(inputs)
-    out = BatchNormalization()(out)
+                 strides=strides, padding=padding,
+                 use_bias=False)(inputs)
+    if batchnorm:
+         out = BatchNormalization()(out)
     if activation is not None:
         out = Activation(activation)(out)
     return out
 
 
-def bn_deconv_layer(inputs, num_filters, kernel_size,
+def bn_deconv_layer(inputs,
+                    num_filters,
+                    kernel_size,
+                    strides,
                     padding='same',
                     activation='relu',
-                    dropout_rate=None):
+                    dropout_rate=None,
+                    batchnorm=True):
     out = Conv2DTranspose(num_filters,
                           kernel_size=kernel_size,
-                          padding=padding)(inputs)
-    out = BatchNormalization()(out)
+                          strides=strides,
+                          padding=padding,
+                          use_bias=False)(inputs)
+    if batchnorm:
+      out = BatchNormalization()(out)
     if activation is not None:
         out = Activation(activation)(out)
     if dropout_rate is not None:
